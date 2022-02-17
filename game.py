888844978eabeeeb32125c39a8b09510d3d1565a
Alice <marchi_alice@yahoo.com>
2022-02-17 23:28:23 +0100

    Final clase jueves

--- game.py
+++ game.py
@@ -2,16 +2,31 @@ import pygame as pg
 pg.init()
 
 class Bola:
-    def __init__(self, x, y, color=(255,255,255), radio =10):
+    def __init__(self, padre, x, y, color=(255,255,255), radio =10):
         self.x = x
         self.y = y
         self.color = color
         self.radio = radio
+        self.vx = 1
+        self.vy = 1
+        self.padre = padre
+
+    def mover(self):
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.x <= self.radio or self.x >= self.padre.get_width() - self.radio:
+            self.vx *= -1
+        if self.y <= self.radio or self.y >= self.padre.get_height() - self.radio:
+            self.vy *= -1
+
+    def dibujar(self):
+        pg.draw.circle(self.padre, self.color,(self.x, self.y), self.radio)
 
 class Game:
     def __init__(self, ancho=600, alto=800):
         self.pantalla = pg.display.set_mode((ancho, alto))
-        self.bola = Bola(ancho//2, alto//2, (255,255,0))
+        self.bola = Bola(self.pantalla, ancho//2, alto//2, (255,255,0))
 
     def bucle_ppal(self):
         game_over = False
@@ -23,10 +38,10 @@ class Game:
                 if evento.type == pg.QUIT:
                     game_over = True 
 
-
+            self.bola.mover()
             self.pantalla.fill((255, 0, 0))
-            pg.draw.circle(self.pantalla, self.bola.color, 
-                        (self.bola.x, self.bola.y), self.bola.radio)
+            self.bola.dibujar()
+            
 
             pg.display.flip()
 
